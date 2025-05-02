e31645f67a62ccd40d2da89cba44bed89e8a1c3e
sudhirskp <gnitwallah@gmail.com>
2025-05-02 23:08:48 +0530

    add hollow inverted triangle pattern implementation in C++

---
+++ learnyard/patterns/hollow_invertwd.cpp
@@ -0,0 +1,30 @@
+// *********
+//  *     *
+//   *   *
+//    * *
+//     *
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 5; // number of levels
+    for (int i = 1; i <= n; i++) {
+        // Printing spaces
+        for (int j = 1; j < i; j++) {
+            cout << " ";
+        }
+        // Printing stars
+        for (int j = 1; j <= (2*n-(2 * i - 1)); j++) {
+            if(i==1 || j==1 || j==(2*n-(2 * i - 1)) || i==n){
+           cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout << endl;
+    
+    }
+    return 0;
+}
