4a4c7dbfa7d6f6e12b6548c0f0da48cfa536a04e
sudhirskp <gnitwallah@gmail.com>
2025-05-07 22:04:11 +0530

    add pyramid and rhombus pattern implementations in C++

---
+++ learnyard/patterns/pyramid.cpp
@@ -0,0 +1,23 @@
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   for(int i=1;i<=5;i++){
+    for(int j=i;j<5;j++){
+        cout<<" ";
+
+    }
+
+    for(int j=1; j<=(2*i-1);j++){
+        cout<<"*";
+   }
+   cout<<endl;
+}
+return 0;
+}
---
+++ learnyard/patterns/rhombus.cpp
@@ -0,0 +1,23 @@
+//     *****
+//    *****
+//   *****
+//  *****
+// *****
+#include<iostream>
+using namespace std;
+int main(){
+    int r ,c ;
+    cout<<" ENter row and col : ";
+    cin>>r>>c;
+    for(int i=1;i<=r;i++){
+        for(int j=1;j<=r-i;j++){
+            cout<<" ";
+        }
+       
+        for(int k=1;k<=c;k++){
+            cout<<"*";
+        }
+        cout<<endl;
+            
+    }
+}
